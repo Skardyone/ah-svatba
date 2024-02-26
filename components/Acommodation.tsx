7aa67a27f4bb6e07eb72ea71d4973d3c9c923c31
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-02-26 17:45:01 +0100

    cc

--- components/Acommodation.tsx
+++ components/Acommodation.tsx
@@ -13,6 +13,7 @@ function Acommodation() {
         </div>
         <div className='w-1/2 h-full bg-bezova flex flex-col items-center justify-around p-12'>
             <h3>ubytovani</h3>
+            
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cupiditate delectus hic cumque nisi voluptate, iusto ratione ad laboriosam repellendus.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cupiditate delectus hic cumque nisi voluptate, iusto ratione ad laboriosam repellendus.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cupiditate delectus hic cumque nisi voluptate, iusto ratione ad laboriosam repellendus.</p>
