beedff663e140bfa10b7381af924ce8c0b209410
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-19 16:34:08 +0100

    people desktoip

--- components/People.tsx
+++ components/People.tsx
@@ -1,9 +1,35 @@
+"use client"
 import React from 'react'
+import { AnimatedTooltip } from "./ui/animated-tooltip"
+
+const people = [
+    {
+      id: 1,
+      name: "Barbora",
+      designation: "fotografka",
+      image:
+        "/fotografka.jpg",
+    },
+    {
+      id: 2,
+      name: "Patrik",
+      designation: "svědek",
+      image:
+        "/svedek.jpg",
+    },
+    {
+      id: 3,
+      name: "Martina",
+      designation: "svědkyně",
+      image:
+        "/svedkyne.jpg",
+    },
+  ];
 
 function People() {
   return (
-    <div className='h-[70vh] w-full flex flex-col items-center justify-center'>
-        ppl
+    <div className='h-[70vh] w-full flex flex-row items-center justify-center'>
+        <AnimatedTooltip items={people}/>
     </div>
   )
 }
