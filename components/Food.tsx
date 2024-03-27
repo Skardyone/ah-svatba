562083233d1bfe597d50a68520d1fa1770818afd
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-27 20:47:33 +0100

    font text xl

--- components/Food.tsx
+++ components/Food.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Food() {
   return (
-    <div className="h-[50vh] w-full px-6 md:w-3/4 xl:w-3/4 font-light text-base md:text-xl xl:text-2xl flex flex-col items-center justify-center border-t-2 border-zelena pb-8">
+    <div className="h-[50vh] w-full px-6 md:w-3/4 xl:w-3/4 font-light text-base md:text-xl xl:text-xl flex flex-col items-center justify-center border-t-2 border-zelena pb-8">
       <p className="">
-        G Během celého dne bude pro všechny zajištěno jídlo a nápoje. Před obřadem
+        Během celého dne bude pro všechny zajištěno jídlo a nápoje. Před obřadem
         bude na vás čekat drobné občerstvení (knoflíky u košile musí vydržet
         přes obřad). Slavnostní hostina bude zahrnovat společnou polévku s
         pestrým rautem. K odpolední kávě bude rozkrojen svatební dort. V
