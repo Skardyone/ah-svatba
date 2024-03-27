0e3af2156a663715f724c0fda11acc80604b45f9
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-27 23:25:40 +0100

    json mending

--- app/dotaznik/Survey.tsx
+++ app/dotaznik/Survey.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
-import { json } from "./dotaznik.json";
+import json from "./dotaznik.json";
 
 function SurveyComponent() {
-    const survey = new Model(json);
+    const survey = new Model(json.json);
     survey.onComplete.add((sender, options) => {
         console.log(JSON.stringify(sender.data, null, 3));
     });
