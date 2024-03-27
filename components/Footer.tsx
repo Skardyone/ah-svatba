90f3fcc04db168386eccaff0e4c0e6b570f9c21f
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-27 21:56:41 +0100

    nadpisy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='h-[70vh] w-full bg-zelena flex flex-col items-center justify-evenly'>
         <div className='w-full px-8 xl:px-0 lg:w-1/2 xl:text-xl text-center font-light border-b-2 border-white pb-12 xl:py-8'>
-          <h3 className='my-6 font-svarge xl:text-[3rem]'>KDYŽ SI NEVÍŠ RADY S DARY</h3>
+          <h3 className='my-6 xl:my-12 font-svarge xl:text-[3rem]'>KDYŽ SI NEVÍŠ RADY S DARY</h3>
             <p>
             Nějaký ten pátek žijeme už společně, proto nové sklenky jsou pro nás již zbytečné. </p> <p> Chcete-li nás obdarovat, přidejte nám na cestu, kde si ženich užije svoji milou nevěstu.
             </p>
