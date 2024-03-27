c533706dbf7a0916b25d3993542696e1b05c478f
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-27 18:08:34 +0100

    day reworked

--- components/Day.tsx
+++ components/Day.tsx
@@ -26,54 +26,64 @@ function Day() {
                 </div>
             </div>
         </div>
-        <div className='md:h-[40vh] w-full grid grid-flow-dense grid-cols-2 md:grid-cols-4 gap-6 md:gap-4  border-t-2 border-bezova py-8 xl:py-8 '>
-            <div className='h-full w-full flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><FaCarAlt /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+        <div className='hidden w-full h-1 bg-bezova xl:flex items-center justify-around'>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+            <div className='h-8 w-8 bg-bezova rounded-full border-[5px] border-white'></div>
+        </div>
+        <div className='md:h-[40vh] w-full grid grid-flow-dense grid-cols-2 md:grid-cols-4 gap-6 md:gap-4 xl:grid-cols-8 xl:gap-0  py-8 xl:py-0 '>
+            <div className='h-full w-full flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><FaCarAlt /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>11:00 - 12:00</p>
                     <p>Příjezd hostů</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><GiLinkedRings /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><GiLinkedRings /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>13:00</p>
                     <p>Svatební obřad</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><MdOutlinePhotoCamera /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><MdOutlinePhotoCamera /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Společné focení</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><GiKnifeFork /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><GiKnifeFork /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Slavnostní hostina</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><GiStairsCake /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><GiStairsCake /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Krájení dortu</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-around'>
-                <span className='text-[3rem]'><PiSneakerMoveBold /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><PiSneakerMoveBold /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Novomanželský tanec</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><MdPhotoCameraFront /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><MdPhotoCameraFront /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Fotokoutek</p>
                 </div>
             </div>
-            <div className='h-full w-full  flex flex-col items-center justify-center'>
-                <span className='text-[3rem]'><GiMusicalNotes /></span>
-                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem]'>
+            <div className='h-full w-full  flex flex-col items-center justify-start'>
+                <span className='text-[3rem] xl:text-[5rem]'><GiMusicalNotes /></span>
+                <div className='flex flex-col items-center justify-center text-center font-bold xl:text-[1rem] 2xl:text-[1.6rem]'>
                     <p>Večerní zábava</p>
                 </div>
             </div>
