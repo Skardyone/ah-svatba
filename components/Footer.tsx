14bc625e81c994ef4e79197a4516b54ae5a89e94
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-27 19:02:35 +0100

    dary

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <div className='h-[70vh] w-full bg-zelena flex flex-col items-center justify-evenly font-bold'>
         <div className='w-full px-8 xl:px-0 lg:w-1/2 xl:text-xl text-center border-b-2 border-white pb-12 xl:py-8'>
+          <h3 className='my-6'>KDYŽ SI NEVÍŠ RADY S DARY</h3>
             <p>
             Nějaký ten pátek žijeme už společně, proto nové sklenky jsou pro nás již zbytečné. Chcete-li nás obdarovat, přidejte nám na cestu, kde si ženich užije svoji milou nevěstu.
             </p>
