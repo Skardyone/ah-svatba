08195df293187ba696897c5dc03fdfbc3f3daf8b
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-28 13:31:41 +0100

    pt rework

--- app/dotaznik/page.tsx
+++ app/dotaznik/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='h-screen w-full pt-8'>
+    <div className='h-screen w-full'>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfH6i3HZTtfKxL7XfqDkhKZ4chi7T_3VH3jHEWKATnQVe74Mw/viewform?embedded=true" className='bg-zelena' width="100%" height="100%" frameBorder="0">Načítání…</iframe>
     </div>
   )
