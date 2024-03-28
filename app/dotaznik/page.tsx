da7797760ab51c01189b3870f6f255cec4a87bec
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-03-28 13:31:03 +0100

    formular

--- app/dotaznik/page.tsx
+++ app/dotaznik/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='h-screen w-full'>
-      <iframe frameBorder="0" id="iframeX6D3A1V8A8F1F9Y6J" src="https://www.survio.com/survey/i/I2M5L5Y4P3F9A5X4F" height="100%" width="100%"></iframe>
+    <div className='h-screen w-full pt-8'>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfH6i3HZTtfKxL7XfqDkhKZ4chi7T_3VH3jHEWKATnQVe74Mw/viewform?embedded=true" className='bg-zelena' width="100%" height="100%" frameBorder="0">Načítání…</iframe>
     </div>
   )
 }
