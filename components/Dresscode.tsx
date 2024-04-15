21a04de30b8e002de30647ff7c7eb8d8cf425782
Skardyone <44971442+Skardyone@users.noreply.github.com>
2024-04-15 12:07:17 +0200

    dress

--- components/Dresscode.tsx
+++ components/Dresscode.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Dresscode() {
   return (
-    <div>
-        
+    <div className="w-full md:w-3/4 xl:w-3/4 font-light text-base md:text-xl xl:text-xl px-6 xl:px-0 my-12 space-y-12">
+      <h2 className="text-center uppercase  m-4 text-[2rem] xl:text-[5rem] font-svarge">
+        dresscode
+      </h2>
+      <p>
+        Věříme v krásné slunečné počasí, takže tomu je nutné přizpůsobit i váš
+        outfit. Pánové mohou nechat saka doma a pohodlně si užít den v košili a
+        kalhotách. Dámy si určitě poradí a najdou ve svém šatníku stylové šaty.
+        Svatební barvy máme, ale ty necháme pro výzdobu. 
+      </p>
+      <p className="text-center">Milé dámy, bílá patří nevěstě!</p>
     </div>
-  )
+  );
 }
 
-export default Dresscode
+export default Dresscode;
